5de4cee1b2a70fad8f5954b8d41aab6ae243e8f6
M3RCURRRY <49792824+M3RCURRRY@users.noreply.github.com>
2022-06-30 12:43:27 +0300

    Chat state

--- react-casino/src/components/Layout/ChatSide/Chat/Chat.js
+++ react-casino/src/components/Layout/ChatSide/Chat/Chat.js
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import styles from "./Chat.module.css";
 
 function Chat() {
+
+  const [msg, setMsg] = useState("");
+
+  function onSubmitMessageHandler(e) {
+    e.preventDefault();
+    setMsg("");
+    console.log(msg);
+  }
+
   return (
     <div className={styles.chatContainer}>
       <div className={styles.chatWrapper}>
@@ -8,7 +18,12 @@ function Chat() {
           <div className={styles.chatLimiter} />
           <div className={styles.chatLabel}>Online-chat</div>
         </div>
-        <div className={styles.chatBox}>123</div>
+        <div className={styles.chatBox}>
+          <form className={styles.msgForm} onSubmit={e => onSubmitMessageHandler(e)}>
+            <input value={msg} type="text" name="name" className={styles.msgTextinput} onChange={e => {setMsg(e.target.value)}}/>
+            <input type="submit" value="send" className={styles.msgSubmit}/>
+          </form>
+        </div>
       </div>
     </div>
   );
