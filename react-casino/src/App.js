d4ad5a96ddb876584de8740f227de4bb4ebf6768
M3RCURRRY <49792824+M3RCURRRY@users.noreply.github.com>
2022-07-04 06:29:41 +0300

    Some new components

--- react-casino/src/App.js
+++ react-casino/src/App.js
@@ -4,66 +4,9 @@ import Button from "./components/controls/Button/Button";
 import ContentArea from "./components/Layout/CasinoSide/ContentArea/ContentArea";
 import Header from "./components/Layout/CasinoSide/Header/Header";
 import ChatSide from "./components/Layout/ChatSide/ChatSide";
+import Roulette from "./components/Layout/GameModes/Roulette/Roulette";
 
 function App() {
-
-  function renderCanvas() {
-    let canvas = document.getElementById("myCanvas");
-
-    let rotated = false;
-
-    canvas.onclick = function() {
-      var div = document.getElementById('myCanvas'),
-        deg = rotated ? 0 : 1066;
-
-      div.style.webkitTransform = 'rotate('+deg+'deg)'; 
-      div.style.mozTransform    = 'rotate('+deg+'deg)'; 
-      div.style.msTransform     = 'rotate('+deg+'deg)'; 
-      div.style.oTransform      = 'rotate('+deg+'deg)'; 
-      div.style.transform       = 'rotate('+deg+'deg)';
-
-      rotated = !rotated;
-    }
-
-    canvas.width = 650;
-    canvas.height = 650;
-
-    let ctx = canvas.getContext("2d");
-
-    //Math.floor(Math.random()*16777215).toString(16)
-
-    function randomHex() {
-      return "#" + Math.floor(Math.random()*16777215).toString(16);
-    }
-
-    drawPieSlice(ctx, 325,325,325, Math.PI, Math.PI/2, randomHex());
-    drawPieSlice(ctx, 325,325,325, Math.PI, Math.PI + Math.PI/4, randomHex());
-    drawPieSlice(ctx, 325,325,325, 0, Math.PI/2, randomHex());
-    drawPieSlice(ctx, 325,325,325, Math.PI/2, Math.PI, randomHex());
-    drawPieSlice(ctx, 325,325,300, 0, Math.PI*2, "#151515");
-  }
-
-  function drawLine(ctx, startX, startY, endX, endY) {
-    ctx.beginPath();
-    ctx.moveTo(startX, startY);
-    ctx.lineTo(endX, endY);
-    ctx.stroke();
-  }
-
-  function drawArc(ctx, centerX, centerY, radius, startAngle, endAngle){
-    ctx.beginPath();
-    ctx.arc(centerX, centerY, radius, startAngle, endAngle);
-    ctx.stroke();
-  }
-
-  function drawPieSlice(ctx,centerX, centerY, radius, startAngle, endAngle, color ){
-    ctx.fillStyle = color;
-    ctx.beginPath();
-    ctx.moveTo(centerX,centerY);
-    ctx.arc(centerX, centerY, radius, startAngle, endAngle);
-    ctx.closePath();
-    ctx.fill();
-  }
 
   return (
     <div className="App">
@@ -72,14 +15,14 @@ function App() {
       <div className="contentContainer">
         <div className="contentWrapper">
           <Header>
-            <Button handler={() => renderCanvas()} content={"Roulette"}/>
+            <Button handler={() => null} content={"Roulette"}/>
             <Button handler={() => console.log("test")} content={"Jackpot"} />
             <Button handler={() => console.log("test")} content={"Landmines"} />
           </Header>
           <ContentArea>
-            123
+            <Roulette/>
           </ContentArea>
-          <canvas id="myCanvas"></canvas>
+          
         </div>
       </div>
     </div>
--- react-casino/src/components/Layout/GameModes/Roulette/Roulette.js
+++ react-casino/src/components/Layout/GameModes/Roulette/Roulette.js
@@ -1,7 +1,90 @@
+import { useState } from "react";
+import styles from "./Roulette.module.css";
+
+function renderCanvas() {
+  let canvas = document.getElementById("myCanvas");
+
+
+  canvas.width = 650;
+  canvas.height = 650;
+
+  let ctx = canvas.getContext("2d");
+  ctx.font = "30px Arial";
+
+  function randomHex() {
+    return "#" + Math.floor(Math.random()*16777215).toString(16);
+  }
+
+  let lastPoint = Math.PI * 3 / 2;
+
+  let currentColor = null;
+  for(let i in Array.from(Array(40).keys())) {    
+    if (i % 21 === 0 && i != 0) currentColor = "gold";
+    else if (i % 5 === 0 && i != 0) currentColor = "#006ebd";
+    else if (i % 2 === 1) currentColor = "red";
+    else currentColor = "#121111";
+
+    let computed = 2 * Math.PI * 1 / 40;
+    drawPieSlice(ctx, 325,325,325, lastPoint, lastPoint + computed, currentColor);
+    lastPoint += computed;
+  }
+
+  drawPieSlice(ctx, 325,325,315, 0, Math.PI*2, "#151515");
+
+  ctx.fillStyle = "red";
+  ctx.fillText("30", 255, 325);
+}
+
+function drawLine(ctx, startX, startY, endX, endY) {
+  ctx.beginPath();
+  ctx.moveTo(startX, startY);
+  ctx.lineTo(endX, endY);
+  ctx.stroke();
+}
+
+function drawArc(ctx, centerX, centerY, radius, startAngle, endAngle){
+  ctx.beginPath();
+  ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+  ctx.stroke();
+}
+
+let rotated = false;
+
+function spinWheel () {
+  var div = document.getElementById('myCanvas'),
+    deg = rotated ? 0 : 1066;
+
+  div.style.webkitTransform = 'rotate('+deg+'deg)'; 
+  div.style.mozTransform    = 'rotate('+deg+'deg)'; 
+  div.style.msTransform     = 'rotate('+deg+'deg)'; 
+  div.style.oTransform      = 'rotate('+deg+'deg)'; 
+  div.style.transform       = 'rotate('+deg+'deg)';
+
+  rotated = !rotated;
+}
+
+function drawPieSlice(ctx,centerX, centerY, radius, startAngle, endAngle, color ){
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(centerX,centerY);
+  ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+  ctx.closePath();
+  ctx.fill();
+}
+
 function Roulette(props) {
+
   return(
-    <div>
-      123
+    <div className={styles.rouletteLayout}>
+      <div className={styles.rouletteContainer}>
+        <div className={styles.roulette}>
+          <canvas id="myCanvas"></canvas>
+        </div>
+        <div className={styles.betManager}>
+          <button onClick={renderCanvas}>Render</button>
+          <button onClick={spinWheel}>Spin</button>
+        </div>
+      </div>
     </div>
   )
 }
